221d6925b31ba9389dd61444be10f2ac4e2c0102
sowmik-sec <sowmik.sec@gmail.com>
2023-10-29 21:08:54 +0600

    pagination in spring

--- src/main/java/com/sowmik/restcruddemo/dao/EmployeeRepository.java
+++ src/main/java/com/sowmik/restcruddemo/dao/EmployeeRepository.java
@@ -4,7 +4,7 @@ import com.sowmik.restcruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(path = "members")
+// @RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
     // that's it ... no need to write any code
 }
